cd37ac6e8568e8e18bde1831401010e3e2fa350d
kashish <singhkashish6962@gmail.com>
2026-08-30 12:12:49 +0530

    added sum program

---
+++ sum.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int num1 ,num2,sum;
+    printf("enter two numbers num1,num2");
+    scanf("%d %d",&num1, &num2);
+    sum=num1+num2;
+    printf("%d", sum);
+    return 0;
+}
