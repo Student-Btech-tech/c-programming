04e8d0dd913a156e9d5c11076169e3723f171e4b
kashish <singhkashish6962@gmail.com>
2026-08-31 12:45:02 +0530

    add an another example of if-else

---
+++ example_If-else.c
@@ -0,0 +1,12 @@
+//find the largest among two numbers
+#include<stdio.h>
+int main(){
+    int a=46, b=37;
+    if(a>b){
+        printf("a is largest");
+    }
+    else{
+        printf("b is largest");
+    }
+     return 0;
+}
