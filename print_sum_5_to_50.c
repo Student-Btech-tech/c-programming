50e9398c2fb6c5dfcd7a0941de053d927bba5015
kashish <singhkashish6962@gmail.com>
2026-09-06 16:13:12 +0530

    added sum program from somewhere to somewhere

---
+++ print_sum_5_to_50.c
@@ -0,0 +1,11 @@
+//Calculate the sum of all numbers b/w 5 and 50 (including 5 & 50)
+#include<stdio.h>
+int main(){
+    int sum =0;
+
+    for(int i=5; i<=50; i++){
+        sum=sum+i;
+    }
+    printf("sum is %d", sum);
+    return 0;
+}
