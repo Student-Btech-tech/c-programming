35a6dfb65d49062d41d5a7f943ebef8bcaac83e6
kashish <singhkashish6962@gmail.com>
2026-09-07 23:59:19 +0530

    added sum program from somewhere to somewhere

---
+++ functio_hello_goodbye.c
@@ -0,0 +1,14 @@
+//write 2 functions Hello 
+#include<stdio.h>
+//declaration/prototype
+void printHello();
+
+//function call
+int main(){
+    printHello();
+    return 0;
+}
+//function definition
+void printHello(){
+    printf("Hello\n");
+}
