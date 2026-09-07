411b63f021f765b18b85cb021b7f97ef199a8e60
kashish <singhkashish6962@gmail.com>
2026-09-08 00:11:15 +0530

    added function_hello_goodbye.c program

--- function_hello_goodbye.c
+++ function_hello_goodbye.c
@@ -1,14 +1,21 @@
-//write 2 functions Hello 
+//write 2 functions Hello & Goodbye
 #include<stdio.h>
 //declaration/prototype
 void printHello();
+void printGoodbye();
 
 //function call
 int main(){
     printHello();
+    printGoodbye();
     return 0;
 }
 //function definition
 void printHello(){
     printf("Hello\n");
 }
+
+void printGoodbye(){
+    printf("Goodbye\n");
+}
+
